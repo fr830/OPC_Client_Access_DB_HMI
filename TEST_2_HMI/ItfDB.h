de73ec59e8c33c5d29d3f1c857a4bead6dff91b8
willheisen <will.pilot42@gmail.com>
2017-11-28 23:04:07 +0100

    Debug file

---
+++ TEST_2_HMI/ItfDB.h
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////
+//
+// ItfDB.h: Definition of Class CItfDB
+//
+//////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <sqlext.h>
+#include <sstream>
+#include "SQLTools.h"
+#include "TypesDef.h"
+
+
+#define MAX_COL_LENGTH 50
+#define MAX_FIELD      75
+
+class CItfDB
+{
+public:
+	CItfDB(void);
+	~CItfDB(void);
+
+	long UpdateDB(typRecipe *recipe);
+
+private:
+
+	// SQL Handles 
+	SQLHENV		m_hndEnv;
+	SQLHDBC		m_hndDBConnect;
+	SQLHSTMT  	m_hndSQLState;
+
+	// SQL Operations
+	CSQLTools	cSQL;
+
+	// Database connection
+	char *m_strODBC_Connection;
+	char *m_strDBUser;			
+	char *m_strDBPassword;
+
+};
